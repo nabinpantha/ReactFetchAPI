93c8f92e2d61755820b25cb21a4daf7578e09b9d
Nabin Pantha <nabinpantha@MacBook-Pro.local>
2021-08-21 22:09:47 -0500

    used-fetch-api

--- learn-api-integration/src/App.js
+++ learn-api-integration/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [books, setBook] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.anapioficeandfire.com/api/books")
+      .then((response) => response.json())
+      .then((data) => setBook(data));
+  }, []);
+
+  //  console.log(books);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Note: This section deals with fetching API using fetch in react function
+        with the help of useEffect() React Hook
+      </p>
+      <h1>Displaying Api data in UI using Fetch call</h1>
+
+      {books && (
+        <>
+          {/* loop over the books */}
+          {books.map((book, index) => (
+            <div key={index}>
+              <h2>Name: {book.name}</h2>
+              <h3>Author: {book.authors[0]}</h3>
+              <h3>Publisher: {book.publisher}</h3>
+              <h3>Release Date: {book.released}</h3>
+              <h3>ISBN: {book.isbn}</h3>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
